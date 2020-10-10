d70cd504a159464851f9a9532c0b93d90bf1ea86
donkeynoodle <36633985+reubenxdsilva@users.noreply.github.com>
2020-10-10 19:05:38 +0200

    Initial commit

---
+++ js/default.js
@@ -0,0 +1,18 @@
+
+
+let windowTop = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+
+});
+
+
+window.addEventListener("scroll", (e) => {
+  windowTop = window.pageYOffset;
+});
+
+
+
+
+
+
