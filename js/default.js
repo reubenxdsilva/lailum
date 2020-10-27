50c8ac8a993fcec03628247e6703332c25681de3
donkeynoodle <36633985+reubenxdsilva@users.noreply.github.com>
2020-10-27 10:33:54 +0100

    get new images + lightbox

--- js/default.js
+++ js/default.js
@@ -12,33 +12,59 @@ window.addEventListener("scroll", (e) => {
 });
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Initialize here and run showSlide() to give your lightbox a default state.
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+let slideIndex = 1;
+showSlide(slideIndex);
+
+// You are providing the functionality for your clickable content, which is 
+// your previews, dots, controls and the close button.
+
+function openLightbox(thisLightbox) {
+  document.getElementById(thisLightbox).style.display = 'block';
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+function closeLightbox(thisLightbox) {
+  document.getElementById(thisLightbox).style.display = 'none';
+};
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// Note that you are assigning new values here to our slidIndex.
 
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+};
 
+function toSlide(n) {
+  showSlide(slideIndex = n);
+};
 
+// This is your logic for the light box. It will decide which slide to show 
+// and which dot is active.
 
+function showSlide(n) {
+  const slides = document.getElementsByClassName('slide');
+  console.log(slides.length);
+  //let modalPreviews = document.getElementsByClassName('modal-preview');
 
+  if (n > slides.length) {
+    slideIndex = 1;	
+  };
+  
+  if (n < 1) {
+    slideIndex = slides.length;
+  };
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  };
+  /*
+  for (let i = 0; i < modalPreviews.length; i++) {
+    modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
+  };
+  */
+  
+  slides[slideIndex - 1].style.display = 'block';
+  //modalPreviews[slideIndex - 1].className += ' active';
+};
